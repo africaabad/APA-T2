5b4e97c9302be7c53139079412d4c0879670f665
africaabad <161127062+africaabad@users.noreply.github.com>
2025-03-25 23:04:13 +0100

    Update primos.py

--- primos.py
+++ primos.py
@@ -28,6 +28,66 @@ def esPrimo(numero):
     
     return True
 
+
+def primos(numero):
+    """
+    retorno una tupla amb tots els num primos menors que el seu argument
+    """
+    res = []
+    for prova in range(2, numero):
+        if esPrimo(prova) == True:
+            res.append(prova)
+
+    return tuple(res)
+
+
+def descompon(numero):
+    """
+    Retorna una tupla amb la descomposició en factors primers del seu argument 
+    """
+    factors = [] 
+    divisor = 2
+    while numero > 1:
+
+        if numero % divisor == 0:
+            numero = numero//divisor 
+            factores.append(divisor)
+            divisor = 2
+        else:
+            divisor += 1
+
+    return tuple(factors) 
+
+
+def mcd(numero1, numero2):
+    #se multiplican factores comunes
+    n1 = descompon(numero1)
+    n2 = descompon(numero2)
+    n1 = set(n1) #convertimos a conjunto (no repetidos)
+    n2 = set(n2)
+    res = 1
+    for prova in n1:
+        if prova in n2:
+            res = res*prova
+    return res
+    
+def mcm(numero1, numero2):
+    #formula de mcd a mcm
+    return (numero1 * numero2) // mcd(numero1, numero2)
+    
+def mcdN(*numeros):
+    resultado = numeros[0]
+    for num in numeros[1:]:
+        resultat = mcd(resultat, num)
+    return resultat
+
+def mcmN(*numeros):    
+    resultat = numeros[0]
+    for num in numeros[1:]:
+        resultat = mcm(resultat, num)
+    return resultat
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
